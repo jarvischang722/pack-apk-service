4420f5cc48cb111fbfc1d0d75a63640986b7d312
Jun Chang <jun.js.tw@tripleonetech.net>
2018-06-26 16:32:19 +0800

    add build.gradle sample file

--- src/route/index.js
+++ src/route/index.js
@@ -1,10 +1,8 @@
 const exempt = require('../authorization/exemptions').add
-const logger = require('log4js').getLogger()
 
 const bind = (route, config) => {
   require('./apk')(route, config, exempt)
   route.get('/', (req, res) => {
-    logger.info(11111)
     res.render('index')
   })
 }
