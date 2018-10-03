207876f15a004de3105991bf0295711855401f0d
Jun Chang <jun.js.tw@tripleonetech.net>
2018-10-03 17:22:31 +0800

    adjust timeout  to 10 mins

--- src/schema/apk.js
+++ src/schema/apk.js
@@ -243,7 +243,7 @@ const build = async (req, callback) => {
 
 const listenBuildApkResult = (req, buildApkProcess, callback) => {
   const { kernel, apk_name_en: apkNameEN } = req.body
-  let timeoutSecs = 360
+  let timeoutSecs = 600
   const apkBuildDirPath = `${config.apk[kernel].rootPath}/app/build/outputs/apk/${apkNameEN}/debug`
   const apkPath = `${apkBuildDirPath}/app-${apkNameEN}-debug.apk`
 
